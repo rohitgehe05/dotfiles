4e5a66225d9694c8b3f3de0d83275e27443ed056
Rohit Gehe <rohit.gehe@efficientbazaar.com>
2018-01-26 10:33:35 +0530

    changed username

--- setup/procedures/remote0.sh
+++ setup/procedures/remote0.sh
@@ -6,10 +6,10 @@ sh -c 'echo "set tabsize 4" >> .nanorc'
 
 sh -c 'echo "set tabstospaces" >> .nanorc'
 
-adduser --disabled-password --gecos "" kensotrabing
+adduser --disabled-password --gecos "" rohitgehe05
 
-usermod -aG sudo kensotrabing
+usermod -aG sudo rohitgehe05
 
-cp .nanorc /home/kensotrabing/
+cp .nanorc /home/rohitgehe05/
 
-mkdir -p /etc/ssh/kensotrabing
+mkdir -p /etc/ssh/rohitgehe05
